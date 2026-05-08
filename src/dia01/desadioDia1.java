8b71fb260b24ae046daba0a5f5760f05d9276196
eduardo ciudad figueredo <eduardociudadfigueredo@gmail.com>
2026-05-07 21:35:27 -0300

    primeiro desafo feito

--- src/dia01/desadioDia1.java
+++ src/dia01/desadioDia1.java
@@ -1,7 +1,18 @@
 package dia01;
 
+import java.util.Scanner;
+
 public class desadioDia1 {
     public static void main(String[] args) {
 
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("digite um numero");
+        int num = scanner.nextInt();
+
+        if (num%2 == 0){
+            System.out.println("seu numero é par");
+        }else {
+            System.out.println("é impar");
+        }
     }
 }
